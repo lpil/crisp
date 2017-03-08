0f664b682d92f636c29918ab3a0c62899b34e1b6
Louis Pilfold <louis@lpil.uk>
2017-03-08 23:56:32 +0000

    Clean up parser error indication

--- src/parser.rs
+++ src/parser.rs
@@ -2,16 +2,50 @@ use std::iter;
 use std::str;
 use super::Node;
 
+#[derive(Debug, PartialEq)]
+pub enum ParseError {
+    BadList,
+}
 
-pub fn parse(input: &String) -> Result<Node, String> {
+enum ParseResult {
+    Ok(Node),
+    None,
+    Err(ParseError),
+}
+
+pub fn parse(input: &String) -> Result<Vec<Node>, ParseError> {
     let mut chars = input.chars().peekable();
+    parse_nodes(&mut chars)
+}
+
+fn parse_nodes(mut chars: &mut iter::Peekable<str::Chars>) -> Result<Vec<Node>, ParseError> {
+    let mut nodes = vec![];
+    loop {
+        chomp(&mut chars);
+        match parse_node(&mut chars) {
+            ParseResult::Err(error) => return Err(error),
+            ParseResult::Ok(node) => nodes.push(node),
+            ParseResult::None => break,
+        }
+    }
+    Ok(nodes)
+}
+
+fn parse_node(mut chars: &mut iter::Peekable<str::Chars>) -> ParseResult {
+    if let Some(atom) = parse_atom(&mut chars) {
+        return ParseResult::Ok(atom);
+    }
+    if let Some(num) = parse_number(&mut chars) {
+        return ParseResult::Ok(num);
+    }
     parse_list(&mut chars)
 }
 
-pub fn parse_atom(chars: &mut iter::Peekable<str::Chars>) -> Result<Node, String> {
+
+fn parse_atom(chars: &mut iter::Peekable<str::Chars>) -> Option<Node> {
     let mut buffer = String::new();
     if !valid_atom_start_char(chars) {
-        return Err("Invalid atom".to_string());
+        return None;
     }
     while let Some(&c) = chars.peek() {
         if !c.is_whitespace() && !c.is_control() && c != '(' && c != ')' {
@@ -22,9 +56,9 @@ pub fn parse_atom(chars: &mut iter::Peekable<str::Chars>) -> Result<Node, String
         }
     }
     match &*buffer {
-        "true" => Ok(Node::True),
-        "false" => Ok(Node::False),
-        _ => Ok(Node::Atom(buffer)),
+        "true" => Some(Node::True),
+        "false" => Some(Node::False),
+        _ => Some(Node::Atom(buffer)),
     }
 }
 
@@ -37,7 +71,7 @@ fn valid_atom_start_char(chars: &mut iter::Peekable<str::Chars>) -> bool {
     }
 }
 
-pub fn parse_number(chars: &mut iter::Peekable<str::Chars>) -> Result<Node, String> {
+fn parse_number(chars: &mut iter::Peekable<str::Chars>) -> Option<Node> {
     let mut point = false;
     let mut nums = String::new();
     while let Some(&c) = chars.peek() {
@@ -53,45 +87,27 @@ pub fn parse_number(chars: &mut iter::Peekable<str::Chars>) -> Result<Node, Stri
         }
     }
     match nums.parse() {
-        Ok(n) => Ok(Node::Float(n)),
-        Err(_) => Err("Invalid float".to_string()),
+        Ok(n) => Some(Node::Float(n)),
+        Err(_) => None,
     }
 }
 
-pub fn parse_list(mut chars: &mut iter::Peekable<str::Chars>) -> Result<Node, String> {
+fn parse_list(mut chars: &mut iter::Peekable<str::Chars>) -> ParseResult {
     if chars.peek() != Some(&'(') {
-        return Err("Invalid list. Expected `(`".to_string());
+        return ParseResult::None;
     }
     chars.next();
     chomp(&mut chars);
-    let elements = parse_elems(&mut chars);
+    let elements = match parse_nodes(&mut chars) {
+        Ok(e) => e,
+        Err(e) => return ParseResult::Err(e),
+    };
     if chars.peek() == Some(&')') {
         chars.next();
-        Ok(Node::List(elements))
+        ParseResult::Ok(Node::List(elements))
     } else {
-        Err("Invalid list. Expected `)`".to_string())
+        ParseResult::Err(ParseError::BadList)
     }
-}
-
-fn parse_elems(mut chars: &mut iter::Peekable<str::Chars>) -> Vec<Node> {
-    let mut elems = vec![];
-    loop {
-        chomp(&mut chars);
-        if let Ok(atom) = parse_atom(&mut chars) {
-            elems.push(atom);
-            continue;
-        }
-        if let Ok(num) = parse_number(&mut chars) {
-            elems.push(num);
-            continue;
-        }
-        if let Ok(list) = parse_list(&mut chars) {
-            elems.push(list);
-            continue;
-        }
-        break;
-    }
-    elems
 }
 
 /// Drop preceeding spaces
@@ -111,185 +127,118 @@ fn chomp(chars: &mut iter::Peekable<str::Chars>) {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use super::ParseError::*;
     use super::super::Node::*;
-
-    // Parse
 
     #[test]
     fn parse_test() {
         let input = "(+ 1 2)".to_string();
-        let res = parse(&input);
-        let nums = vec![Atom("+".to_string()), Float(1), Float(2)];
-        let sexpr = List(nums);
-        assert_eq!(res, Ok(sexpr));
+        let list = List(vec![Atom("+".to_string()), Float(1), Float(2)]);
+        assert_eq!(parse(&input), Ok(vec![list]));
     }
 
-    // parse_list
+
+    #[test]
+    fn parse_top_level_values() {
+        let input = "() 1 /".to_string();
+        assert_eq!(parse(&input),
+                   Ok(vec![List(vec![]), Float(1), Atom("/".to_string())]));
+    }
 
     #[test]
     fn parse_list_empty() {
-        let mut chars = "".chars().peekable();
-        let res = parse_list(&mut chars);
-        assert_eq!(res, Err("Invalid list. Expected `(`".to_string()));
+        assert_eq!(parse(&"".to_string()), Ok(vec![]));
     }
 
     #[test]
     fn parse_list_of_num() {
-        let mut chars = "(123)".chars().peekable();
-        let res = parse_list(&mut chars);
-        let sexpr = List(vec![Float(123)]);
-        assert_eq!(res, Ok(sexpr));
+        assert_eq!(parse(&"(123)".to_string()),
+                   Ok(vec![List(vec![Float(123)])]));
     }
 
     #[test]
     fn parse_incomplete_list() {
-        let mut chars = "(123".chars().peekable();
-        let res = parse_list(&mut chars);
-        assert_eq!(res, Err("Invalid list. Expected `)`".to_string()));
+        assert_eq!(parse(&"(123".to_string()), Err(BadList));
     }
 
 
     #[test]
     fn parse_multi_num_list() {
-        let mut chars = "(1 2 3)".chars().peekable();
-        let res = parse_list(&mut chars);
-        let nums = vec![Float(1), Float(2), Float(3)];
-        let sexpr = List(nums);
-        assert_eq!(res, Ok(sexpr));
+        assert_eq!(parse(&"(1 2 3)".to_string()),
+                   Ok(vec![List(vec![Float(1), Float(2), Float(3)])]));
     }
 
     #[test]
     fn parse_nested_list() {
-        let mut chars = "(1 (3))".chars().peekable();
-        let res = parse_list(&mut chars);
-        let sexpr1 = List(vec![Float(3)]);
-        let sexpr2 = List(vec![Float(1), sexpr1]);
-        assert_eq!(res, Ok(sexpr2));
-    }
-
-    // parse_number
-
-    #[test]
-    fn parse_number_empty() {
-        let mut chars = "".chars().peekable();
-        let res = parse_number(&mut chars);
-        assert_eq!(res, Err("Invalid float".to_string()));
-    }
-
-    #[test]
-    fn parse_number_invalid() {
-        let mut chars = "o".chars().peekable();
-        let res = parse_number(&mut chars);
-        assert_eq!(res, Err("Invalid float".to_string()));
-    }
-
-    #[test]
-    fn parse_number_digit_then_letter() {
-        let mut chars = "11o".chars().peekable();
-        let res = parse_number(&mut chars);
-        assert_eq!(res, Ok(Float(11)));
-        assert_eq!(chars.peek(), Some(&'o'));
+        assert_eq!(parse(&"(1 (3))".to_string()),
+                   Ok(vec![List(vec![Float(1), List(vec![Float(3)])])]));
     }
 
     #[test]
     fn parse_number_1_digit() {
-        let mut chars = "5".chars().peekable();
-        let res = parse_number(&mut chars);
-        assert_eq!(res, Ok(Float(5)));
+        assert_eq!(parse(&"5".to_string()), Ok(vec![Float(5)]));
     }
 
     #[test]
     fn parse_number_2_digits() {
-        let mut chars = "52".chars().peekable();
-        let res = parse_number(&mut chars);
-        assert_eq!(res, Ok(Float(52)));
+        assert_eq!(parse(&"52".to_string()), Ok(vec![Float(52)]));
     }
-
-    #[test]
-    fn parse_number_3_digits() {
-        let mut chars = "524   ".chars().peekable();
-        let res = parse_number(&mut chars);
-        assert_eq!(res, Ok(Float(524)));
-    }
-
-    // parse_atom
 
     #[test]
     fn parse_atom_lowercase() {
-        let mut chars = "hello".chars().peekable();
-        let res = parse_atom(&mut chars);
-        assert_eq!(res, Ok(Atom("hello".to_string())));
+        assert_eq!(parse(&"hello".to_string()),
+                   Ok(vec![Atom("hello".to_string())]));
     }
 
     #[test]
     fn parse_atom_uppercase() {
-        let mut chars = "HELLO".chars().peekable();
-        let res = parse_atom(&mut chars);
-        assert_eq!(res, Ok(Atom("HELLO".to_string())));
+        assert_eq!(parse(&"HELLO".to_string()),
+                   Ok(vec![Atom("HELLO".to_string())]));
     }
 
     #[test]
     fn parse_atom_mixed_case() {
-        let mut chars = "HelLO".chars().peekable();
-        let res = parse_atom(&mut chars);
-        assert_eq!(res, Ok(Atom("HelLO".to_string())));
+        assert_eq!(parse(&"HelLO".to_string()),
+                   Ok(vec![Atom("HelLO".to_string())]));
     }
 
     #[test]
     fn parse_atom_with_dash() {
-        let mut chars = "hi-there".chars().peekable();
-        let res = parse_atom(&mut chars);
-        assert_eq!(res, Ok(Atom("hi-there".to_string())));
+        assert_eq!(parse(&"hi-there".to_string()),
+                   Ok(vec![Atom("hi-there".to_string())]));
     }
 
     #[test]
     fn parse_atom_with_underscope() {
-        let mut chars = "what_up".chars().peekable();
-        let res = parse_atom(&mut chars);
-        assert_eq!(res, Ok(Atom("what_up".to_string())));
+        assert_eq!(parse(&"hi_there".to_string()),
+                   Ok(vec![Atom("hi_there".to_string())]));
     }
 
     #[test]
     fn parse_atom_with_other_chars() {
-        let mut chars = "chars1234567890<~>!?\\/:;@#".chars().peekable();
-        let res = parse_atom(&mut chars);
-        assert_eq!(res, Ok(Atom("chars1234567890<~>!?\\/:;@#".to_string())));
+        assert_eq!(parse(&"chars1234567890<~>!?\\/:;@#".to_string()),
+                   Ok(vec![Atom("chars1234567890<~>!?\\/:;@#".to_string())]));
     }
 
     #[test]
     fn parse_atom_true() {
-        let mut chars = "true".chars().peekable();
-        let res = parse_atom(&mut chars);
-        assert_eq!(res, Ok(True));
+        assert_eq!(parse(&"true".to_string()), Ok(vec![True]));
     }
 
     #[test]
     fn parse_atom_false() {
-        let mut chars = "false".chars().peekable();
-        let res = parse_atom(&mut chars);
-        assert_eq!(res, Ok(False));
+        assert_eq!(parse(&"false".to_string()), Ok(vec![False]));
     }
 
-    #[test]
-    fn parse_atom_blacklisted_starts() {
-        assert!(parse_atom(&mut "0".chars().peekable()).is_err());
-        assert!(parse_atom(&mut "1".chars().peekable()).is_err());
-        assert!(parse_atom(&mut "2".chars().peekable()).is_err());
-        assert!(parse_atom(&mut "3".chars().peekable()).is_err());
-        assert!(parse_atom(&mut "4".chars().peekable()).is_err());
-        assert!(parse_atom(&mut "5".chars().peekable()).is_err());
-        assert!(parse_atom(&mut "6".chars().peekable()).is_err());
-        assert!(parse_atom(&mut "7".chars().peekable()).is_err());
-        assert!(parse_atom(&mut "8".chars().peekable()).is_err());
-        assert!(parse_atom(&mut "9".chars().peekable()).is_err());
-        assert!(parse_atom(&mut "(".chars().peekable()).is_err());
-        assert!(parse_atom(&mut ")".chars().peekable()).is_err());
-        assert!(parse_atom(&mut "[".chars().peekable()).is_err());
-        assert!(parse_atom(&mut "]".chars().peekable()).is_err());
-        assert!(parse_atom(&mut "{".chars().peekable()).is_err());
-        assert!(parse_atom(&mut "}".chars().peekable()).is_err());
-        assert!(parse_atom(&mut "'".chars().peekable()).is_err());
-        assert!(parse_atom(&mut "`".chars().peekable()).is_err());
-        assert!(parse_atom(&mut "\"".chars().peekable()).is_err());
-    }
+    // TODO: BadNode errors for reserved chars
+    // #[test]
+    // fn parse_atom_blacklisted_starts() {
+    //     assert_eq!(parse(&mut "[".to_string()), Ok(vec![]));
+    //     assert_eq!(parse(&mut "]".to_string()), Ok(vec![]));
+    //     assert_eq!(parse(&mut "{".to_string()), Ok(vec![]));
+    //     assert_eq!(parse(&mut "}".to_string()), Ok(vec![]));
+    //     assert_eq!(parse(&mut "'".to_string()), Ok(vec![]));
+    //     assert_eq!(parse(&mut "`".to_string()), Ok(vec![]));
+    //     assert_eq!(parse(&mut "\"".to_string()), Ok(vec![]));
+    // }
 }
